5bd83a93a926ecf35917b9ded2dddd6eb421f339
Quadotenshi <quadawosanya@gmail.com>
2023-03-09 14:42:13 -0800

    adjested the policy

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,9 +3,9 @@ import random
 
 number = random.randint(-10, 10)
 
-if number == 0:
-    print(f"{number} is zero\n")
-elif number < 0:
-    print(f"{number} is negative\n")
-else:
+if number > 0:
     print(f"{number} is positive\n")
+elif number == 0:
+    print(f"{number} is zero\n")
+else:
+    print(f"{number} is negative\n")
