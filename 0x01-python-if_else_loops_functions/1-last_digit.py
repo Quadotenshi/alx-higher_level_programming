490cbb64fecdbd9d5c290005bf27bdb36d1e6ac2
Quadotenshi <quadawosanya@gmail.com>
2023-03-10 15:03:38 -0800

    aligning with pycodestyle

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,18 +3,21 @@ import random
 
 number = random.randint(-10000, 10000)
 digit = number % 10
+string = "Last digit of "
+not_six = "and is less than 6 but not 0"
+five = "and is greater than 5"
 
 if number < 0:
-    digit = abs(number) % 10
+    digit = -(abs(number) % 10)
     if digit == 0:
-        print("Last digit of {} is 0 and is 0".format(number))
+        print("{}{} is 0 and is 0".format(string, number))
     else:
-        print("Last digit of {} is -{} and is less than 6 but not 0".format(number, digit))
+        print("{}{} is {} {}".format(string, number, digit, not_six))
 else:
     digit = number % 10
     if digit > 5:
-        print("Last digit of {} is {} and is greater than 5".format(number, digit))
+        print("Last digit of {} is {} {}".format(number, digit, five))
     elif digit == 0:
         print("Last digit of {} is {} and is 0".format(number, digit))
     else:
-        print("Last digit of {} is {} and is less than 6 but not 0".format(number,digit))
+        print("Last digit of {} is {} {}".format(number, digit, not_six))
