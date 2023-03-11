bc8732f127e773857e1518853157f4a95af6d405
Quadotenshi <quadawosanya@gmail.com>
2023-03-11 13:58:09 -0800

    sought help from Busola

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    if number < 0:
-        return abs(number) % 10
-    else:
-        return number % 10
+    print(abs(number) % 10)
+    return abs(number) % 10
