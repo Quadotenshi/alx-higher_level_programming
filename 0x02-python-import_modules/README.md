37136d663683dcd6ead288af905ccc1f3db28a12
quadotenshi <quadawosanya@gmail.com>
2023-03-12 21:13:30 +0000

    import function

---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,3 @@
+this project contains tasks on the use of modules,
+how they are created and imported
+and some other python concepts
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def add(a, b):
+
+	a = 1
+	b = 2
+
+	print("first variable is : {}".format(a))
+	print("second variable is: {}".format(b))
+
+	print("{} + {} = {}".format(a, b, a + b))
+
+	return a + b
