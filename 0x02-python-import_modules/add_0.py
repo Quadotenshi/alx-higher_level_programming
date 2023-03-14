d98288a8823a595363151c88f02e7a2af0b2b9e5
quadotenshi <quadawosanya@gmail.com>
2023-03-14 14:50:19 -0700

    simple function from simple file

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 
 def add(a, b):
-
-	a = 1
-	b = 2
-
-	print("first variable is : {}".format(a))
-	print("second variable is: {}".format(b))
-
-	print("{} + {} = {}".format(a, b, a + b))
-
-	return a + b
+    
+    a = 1
+    b = 2
+    print("a = {}".format(a))
+    print("b = {}".format(b))
+    print("{} + {} = {}".format(a, b, a + b))
+    return a + b
