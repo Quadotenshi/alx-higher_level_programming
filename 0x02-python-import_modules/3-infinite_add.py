c09050e9b3a2d00f8650f6e1f2bfba5bd8b10f64
quadotenshi <quadawosanya@gmail.com>
2023-03-24 16:03:39 -0700

    infinite additon

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    num = len(sys.argv)
+    sum = 0
+
+    for i in range(1, num):
+        sum = sum + int(sys.argv[i])
+    print("{}".format(sum))
