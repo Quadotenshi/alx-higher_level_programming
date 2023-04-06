051cc46b8152bbd60497ac81c70bbeb0b30eed62
quadotenshi <quadawosanya@gmail.com>
2023-04-06 04:56:17 -0700

    adjustment

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    new_list = []
     for item in my_list:
-        return item % 2 == 0
+        new_list.append(item % 2 == 0)
+    return new_list
