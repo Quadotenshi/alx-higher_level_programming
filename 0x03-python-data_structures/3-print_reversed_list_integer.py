9da6de0b0457e6c5a82b281715238b1b1d783453
quadotenshi <quadawosanya@gmail.com>
2023-04-05 17:04:43 -0700

    change style of loop

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for item in my_list:
-        print("{}".format(item))
+    count = len(my_list) - 1
+    while count >= 0:
+        print("{}".format(my_list[count]))
+        count = count - 1
