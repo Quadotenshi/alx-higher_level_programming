276e36b4f304c96a94293812ca46f60110911f0f
quadotenshi <quadawosanya@gmail.com>
2023-04-05 18:21:28 -0700

    bugs fixed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for item in matrix:
         for obj in item:
-            print("{}",end=''.format(obj))
+            print("{}".format(obj,end=' '))
         print()
