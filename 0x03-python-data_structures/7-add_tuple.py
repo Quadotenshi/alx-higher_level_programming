563bb85e4ecf4d927c7caadf7a8ca447572dd5e2
quadotenshi <quadawosanya@gmail.com>
2023-04-05 19:10:08 -0700

    tuple addition

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python7
+def add_tuple(tuple_a=(), tuple_b()):
+    tuple_a + (0,0)
+    tuple_b + (0,0)
+    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_[1])
+    return new_tuple
