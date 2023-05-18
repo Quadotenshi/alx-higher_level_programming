8b12fc37ef095c9119c6cfea8c676f8f02c67824
quadotenshi <quadawosanya@gmail.com>
2023-05-18 06:40:14 -0700

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Creating the square class"""
+class Square:
+    """set the __init__ method with the size field"""
+    def __init__(self, size):
+        """intantiate the private attribute"""
+        self.__size = size
